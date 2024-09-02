79420fd643a3460832f16cd3a2e265fd0ced9b12
Purity Wambua <puritywamb@gmail.com>
2024-09-02 18:49:30 +0300

    anagram detector

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,14 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.base_word = word
+    
+    def match(self, possible_anagrams):
+        # Sort the letters of the base_word
+        sorted_base_word = ''.join(sorted(self.base_word))
+        matches = []
+        for word in possible_anagrams:
+            # Sort the letters of the current word
+            if ''.join(sorted(word)) == sorted_base_word:
+                matches.append(word)
+        return matches
